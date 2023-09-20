d2adc01fad1d2e2e5d3b826640947a66cd556a1c
Max Base <maxbasecode@gmail.com>
2023-09-20 14:06:47 +0100

    ok

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
 // Create a WebSocket connection
 const socket = new WebSocket('ws://localhost:8080');
 
+// Get references to the message list and input field
+const messageList = document.getElementById('messageList');
+const messageInput = document.getElementById('messageInput');
+
 // Event listener for when the connection is opened
 socket.addEventListener('open', (event) => {
   console.log('Connected to WebSocket server');
@@ -9,6 +13,8 @@ socket.addEventListener('open', (event) => {
 // Event listener for messages from the server
 socket.addEventListener('message', (event) => {
   console.log(`Received from server: ${event.data}`);
+
+  appendToList(event.data);
 });
 
 // Event listener for when the connection is closed
@@ -26,8 +32,23 @@ socket.addEventListener('error', (event) => {
 });
 
 // Send a message to the server
+function appendToList(message) {
+  // Create a new list item to display the message
+  const listItem = document.createElement('li');
+  listItem.textContent = message;
+
+  // Append the list item to the message list
+  messageList.appendChild(listItem);
+
+  // Scroll to the bottom of the message list to show the latest message
+  messageList.scrollTop = messageList.scrollHeight;
+}
 function sendMessage() {
-  const message = document.getElementById('messageInput').value;
-  console.log(message);
+  const message = messageInput.value;
   socket.send(message);
+
+  // Clear the input field after sending the message
+  messageInput.value = '';
+
+  appendToList(message);
 }
